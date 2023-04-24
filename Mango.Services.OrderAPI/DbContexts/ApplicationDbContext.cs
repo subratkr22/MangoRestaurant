1781580ece9fae4576b88a656b66526670885c2f
subratkr22 <subratkr22@gmail.com>
2023-04-24 14:56:26 +0530

    Add Order API Microservice

---
+++ Mango.Services.OrderAPI/DbContexts/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Mango.Services.OrderAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Mango.Services.OrderAPI.DbContexts
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<OrderDetails> OrderDetails { get; set; }
+        public DbSet<OrderHeader> OrderHeaders { get; set; }
+    }
+}
